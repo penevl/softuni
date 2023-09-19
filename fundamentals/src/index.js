6e6cfcf185675a51d3289a07f53280f18592f616
penevl <penevl@protonmail.com>
2023-09-19 20:24:02 +0300

    Vacation

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,28 +1,73 @@
-function solve(num) {
-    if (num % 10 == 0) {
-        console.log("The number is divisible by 10");
-        return;
-    }
-    if (num % 7 == 0) {
-        console.log("The number is divisible by 7");
-        return;
-    }
-    if (num % 6 == 0) {
-        console.log("The number is divisible by 6");
-        return;
-    }
-    if (num % 3 == 0) {
-        console.log("The number is divisible by 3");
-        return;
-    }
-    if (num % 2 == 0) {
-        console.log("The number is divisible by 2");
-        return;
-    }
-    console.log("Not divisible");
-}
+function solve(count, type, day) {
+    switch (type) {
+        case "Students":
+            switch (day) {
+                case "Friday":
+                    var sum = 8.45 * count;
+                    if (count >= 30) sum = sum * 0.85;
 
-const test = [30, 15, 12, 1643];
-test.forEach((x) => {
-    solve(x);
-});
+                    console.log(`Total price: ${sum.toFixed(2)}`);
+                    break;
+                case "Saturday":
+                    var sum = 9.8 * count;
+                    if (count >= 30) sum = sum * 0.85;
+
+                    console.log(`Total price: ${sum.toFixed(2)}`);
+                    break;
+                case "Sunday":
+                    var sum = 10.46 * count;
+                    if (count >= 30) sum = sum * 0.85;
+
+                    console.log(`Total price: ${sum.toFixed(2)}`);
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "Business":
+            switch (day) {
+                case "Friday":
+                    if (count >= 100) count = count - 10;
+                    var sum = 10.9 * count;
+                    console.log(`Total price: ${sum.toFixed(2)}`);
+                    break;
+                case "Saturday":
+                    if (count >= 100) count = count - 10;
+                    var sum = 15.6 * count;
+                    console.log(`Total price: ${sum.toFixed(2)}`);
+                    break;
+                case "Sunday":
+                    if (count >= 100) count = count - 10;
+                    var sum = 16 * count;
+                    console.log(`Total price: ${sum.toFixed(2)}`);
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "Regular":
+            switch (day) {
+                case "Friday":
+                    var sum = 15 * count;
+                    if (count >= 10 && count <= 20) sum = sum * 0.95;
+                    console.log(`Total price: ${sum.toFixed(2)}`);
+                    break;
+                case "Saturday":
+                    var sum = 20 * count;
+                    if (count >= 10 && count <= 20) sum = sum * 0.95;
+                    console.log(`Total price: ${sum.toFixed(2)}`);
+                    break;
+                case "Sunday":
+                    var sum = 22.5 * count;
+                    if (count >= 10 && count <= 20) sum = sum * 0.95;
+                    console.log(`Total price: ${sum.toFixed(2)}`);
+                    break;
+                default:
+                    break;
+            }
+            break;
+
+        default:
+            break;
+    }
+}
