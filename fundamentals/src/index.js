0355e6ffdfde1dc457bc926be9386aaa39bf6d7b
penevl <penevl@protonmail.com>
2023-09-19 20:27:38 +0300

    Vacation

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,73 +1,10 @@
-function solve(count, type, day) {
-    switch (type) {
-        case "Students":
-            switch (day) {
-                case "Friday":
-                    var sum = 8.45 * count;
-                    if (count >= 30) sum = sum * 0.85;
-
-                    console.log(`Total price: ${sum.toFixed(2)}`);
-                    break;
-                case "Saturday":
-                    var sum = 9.8 * count;
-                    if (count >= 30) sum = sum * 0.85;
-
-                    console.log(`Total price: ${sum.toFixed(2)}`);
-                    break;
-                case "Sunday":
-                    var sum = 10.46 * count;
-                    if (count >= 30) sum = sum * 0.85;
-
-                    console.log(`Total price: ${sum.toFixed(2)}`);
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case "Business":
-            switch (day) {
-                case "Friday":
-                    if (count >= 100) count = count - 10;
-                    var sum = 10.9 * count;
-                    console.log(`Total price: ${sum.toFixed(2)}`);
-                    break;
-                case "Saturday":
-                    if (count >= 100) count = count - 10;
-                    var sum = 15.6 * count;
-                    console.log(`Total price: ${sum.toFixed(2)}`);
-                    break;
-                case "Sunday":
-                    if (count >= 100) count = count - 10;
-                    var sum = 16 * count;
-                    console.log(`Total price: ${sum.toFixed(2)}`);
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case "Regular":
-            switch (day) {
-                case "Friday":
-                    var sum = 15 * count;
-                    if (count >= 10 && count <= 20) sum = sum * 0.95;
-                    console.log(`Total price: ${sum.toFixed(2)}`);
-                    break;
-                case "Saturday":
-                    var sum = 20 * count;
-                    if (count >= 10 && count <= 20) sum = sum * 0.95;
-                    console.log(`Total price: ${sum.toFixed(2)}`);
-                    break;
-                case "Sunday":
-                    var sum = 22.5 * count;
-                    if (count >= 10 && count <= 20) sum = sum * 0.95;
-                    console.log(`Total price: ${sum.toFixed(2)}`);
-                    break;
-                default:
-                    break;
-            }
-            break;
-
-        default:
-            break;
-    }
+function solve(year) {
+    if ((year % 4 == 0 && !(year && 100 == 0)) || year % 400 == 0)
+        console.log("yes");
+    return;
+    console.log("no");
 }
+
+solve(1984);
+solve(2003);
+solve(4);
