4fad979d4ff7bf597d8eeec75ac984af58becbc3
penevl <penevl@protonmail.com>
2023-09-19 20:31:32 +0300

    Leap year

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,10 +1,11 @@
 function solve(year) {
-    if ((year % 4 == 0 && !(year && 100 == 0)) || year % 400 == 0)
+    if (year % 4 == 0 && !(year % 100 == 0)) {
         console.log("yes");
-    return;
+        return;
+    }
+    if (year % 400 == 0) {
+        console.log("yes");
+        return;
+    }
     console.log("no");
 }
-
-solve(1984);
-solve(2003);
-solve(4);
