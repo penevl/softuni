272d24a672232d9ff5ebcbedd74dea89128fd9ae
penevl <penevl@protonmail.com>
2023-09-19 20:44:26 +0300

    Multiplication table

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,11 +1,12 @@
 function solve(n) {
-    for (let index = 1; index <= n; index++) {
-        var result = "";
-        for (let x = 0; x < index; x++) {
-            result = result + index + " ";
-        }
-        console.log(result);
-    }
+    console.log(`${n} X ${1} = ${n * 1}`);
+    console.log(`${n} X ${2} = ${n * 2}`);
+    console.log(`${n} X ${3} = ${n * 3}`);
+    console.log(`${n} X ${4} = ${n * 4}`);
+    console.log(`${n} X ${5} = ${n * 5}`);
+    console.log(`${n} X ${6} = ${n * 6}`);
+    console.log(`${n} X ${7} = ${n * 7}`);
+    console.log(`${n} X ${8} = ${n * 8}`);
+    console.log(`${n} X ${9} = ${n * 9}`);
+    console.log(`${n} X ${10} = ${n * 10}`);
 }
-
-solve(6);
