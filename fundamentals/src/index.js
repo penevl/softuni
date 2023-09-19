29fd657520c02df92687a9140b8df205662c7ff6
penevl <penevl@protonmail.com>
2023-09-19 19:33:48 +0300

    Sum of odd numbers

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,6 +1,19 @@
-function solve(m, n) {
-    while (m >= n) {
-        console.log(m);
-        m--;
+function solve(n) {
+    var count = 0;
+    var counter = 0;
+    var shouldRun = true;
+    var sum = 0;
+    while (shouldRun) {
+        counter++;
+        // console.log(count, counter, sum)
+        if (!(counter % 2 == 0)) {
+            count++;
+            console.log(counter);
+            sum += counter;
+        }
+        if (count == n) {
+            shouldRun = false;
+        }
     }
+    console.log(`Sum: ${sum}`);
 }
