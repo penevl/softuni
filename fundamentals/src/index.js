9bcc8f6622674b632b6727a6136d6ae5dc5398b1
penevl <penevl@protonmail.com>
2023-09-19 19:12:30 +0300

    Divisible by 3

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,5 +1,5 @@
 function solve() {
-    for (let index = 1; index <= 5; index++) {
-        console.log(index);
+    for (let index = 1; index <= 100; index++) {
+        if (index % 3 == 0) console.log(index);
     }
 }
