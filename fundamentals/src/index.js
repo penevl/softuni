a29c297270b92f110dbff2cfeffdeb876d53a6ea
penevl <penevl@protonmail.com>
2023-09-19 20:54:51 +0300

    Simple login

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,12 +1,19 @@
 function solve(n) {
-    console.log(`${n} X ${1} = ${n * 1}`);
-    console.log(`${n} X ${2} = ${n * 2}`);
-    console.log(`${n} X ${3} = ${n * 3}`);
-    console.log(`${n} X ${4} = ${n * 4}`);
-    console.log(`${n} X ${5} = ${n * 5}`);
-    console.log(`${n} X ${6} = ${n * 6}`);
-    console.log(`${n} X ${7} = ${n * 7}`);
-    console.log(`${n} X ${8} = ${n * 8}`);
-    console.log(`${n} X ${9} = ${n * 9}`);
-    console.log(`${n} X ${10} = ${n * 10}`);
+    var tries = 0;
+    const user = n[0];
+    const passwd = user.split("").reverse().join("");
+    n.splice(0, 1);
+    n.forEach((x) => {
+        tries++;
+        if (tries >= 4) {
+            console.log(`User ${user} blocked!`);
+            return;
+        }
+        if (x == passwd) {
+            console.log(`User ${user} logged in.`);
+            return;
+        } else {
+            console.log("Incorrect password. Try again.");
+        }
+    });
 }
