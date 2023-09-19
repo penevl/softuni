d14bdd10534f16aa4e01488a2c7ea3a28fccab19
penevl <penevl@protonmail.com>
2023-09-19 19:56:04 +0300

    Division

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,5 +1,28 @@
-function solve(num, precission) {
-    if (precission > 15) precission = 15;
-
-    console.log(parseFloat(num.toFixed(precission)));
+function solve(num) {
+    if (num % 10 == 0) {
+        console.log("The number is divisible by 10");
+        return;
+    }
+    if (num % 7 == 0) {
+        console.log("The number is divisible by 7");
+        return;
+    }
+    if (num % 6 == 0) {
+        console.log("The number is divisible by 6");
+        return;
+    }
+    if (num % 3 == 0) {
+        console.log("The number is divisible by 3");
+        return;
+    }
+    if (num % 2 == 0) {
+        console.log("The number is divisible by 2");
+        return;
+    }
+    console.log("Not divisible");
 }
+
+const test = [30, 15, 12, 1643];
+test.forEach((x) => {
+    solve(x);
+});
