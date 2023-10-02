1f8fc87355036a03717d2a6ab57d4476d744288f
penevl <penevl@protonmail.com>
2023-10-02 10:16:20 +0300

    Merge arrays

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,18 +1,23 @@
+/**
+ *
+ * @param {String[]} b
+ * @param {String[]} a
+ */
 function solve(a, b) {
-    if (a.length >= b.length) {
-        a.forEach((element) => {
-            if (b.includes(element)) console.log(element);
-        });
-    } else {
-        b.forEach((element) => {
-            if (a.includes(element)) console.log(element);
-        });
-    }
-    
+    let newArr = [];
+    let output = "";
+    a.forEach((element, index) => {
+        if (index % 2 == 0) {
+            newArr.push(Number(element) + Number(b[index]));
+        } else {
+            newArr.push(element + b[index]);
+        }
+    });
+    newArr.forEach((element) => {
+        output += element + " - ";
+    });
+    console.log(output.substring(0, output.length - 3));
 }
 
-solve(
-    ["Hey", "hello", 2, 4, "Peter", "e"],
-    ["Petar", 10, "hey", 4, "hello", "2"]
-);
-solve(["S", "o", "f", "t", "U", "n", "i", " "], ["s", "o", "c", "i", "a", "l"]);
+solve(["5", "15", "23", "56", "35"], ["17", "22", "87", "36", "11"]);
+solve(["13", "12312", "5", "77", "4"], ["22", "333", "5", "122", "44"]);
