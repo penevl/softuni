4f06ade32be1e1109b0602e5a9cec750b65255ef
penevl <penevl@protonmail.com>
2023-10-02 10:06:07 +0300

    Common elements

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,22 +1,18 @@
-function solve(arr) {
-    const oldArr = Array.from(arr);
-    const newArr = arr;
-    let sumOld = 0;
-    let sumNew = 0;
-    newArr.forEach((num, i) => {
-        if (newArr[i] % 2 == 0) {
-            newArr[i] += i;
-        } else {
-            newArr[i] -= i;
-        }
-    });
-    oldArr.forEach((num) => {
-        sumOld += num;
-    });
-    newArr.forEach((num) => {
-        sumNew += num;
-    });
-    console.log(arr);
-    console.log(sumOld);
-    console.log(sumNew);
+function solve(a, b) {
+    if (a.length >= b.length) {
+        a.forEach((element) => {
+            if (b.includes(element)) console.log(element);
+        });
+    } else {
+        b.forEach((element) => {
+            if (a.includes(element)) console.log(element);
+        });
+    }
+    
 }
+
+solve(
+    ["Hey", "hello", 2, 4, "Peter", "e"],
+    ["Petar", 10, "hey", 4, "hello", "2"]
+);
+solve(["S", "o", "f", "t", "U", "n", "i", " "], ["s", "o", "c", "i", "a", "l"]);
