1afdc20f16b7c35c8f282c282edf7adc10294d9e
penevl <penevl@protonmail.com>
2023-10-02 09:58:23 +0300

    Add and subtract

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,9 +1,22 @@
-function solve(a) {
-    a = String(a);
-    if (a == a.toUpperCase()) {
-        console.log("upper-case");
-    }
-    if (a == a.toLowerCase()) {
-        console.log("lower-case");
-    }
+function solve(arr) {
+    const oldArr = Array.from(arr);
+    const newArr = arr;
+    let sumOld = 0;
+    let sumNew = 0;
+    newArr.forEach((num, i) => {
+        if (newArr[i] % 2 == 0) {
+            newArr[i] += i;
+        } else {
+            newArr[i] -= i;
+        }
+    });
+    oldArr.forEach((num) => {
+        sumOld += num;
+    });
+    newArr.forEach((num) => {
+        sumNew += num;
+    });
+    console.log(arr);
+    console.log(sumOld);
+    console.log(sumNew);
 }
