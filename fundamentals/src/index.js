523d60335c4cc13fc2e9eea6bd35350223488432
penevl <penevl@protonmail.com>
2023-10-02 13:14:23 +0300

    Array rotation - Kinds broken

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,31 +1,10 @@
 /**
  *
- * @param {Number[]} b
  * @param {Number} a
  */
-function solve(a, b) {
-    let out = "";
-    if (a.length >= b) {
-        for (let i = b; i < a.length; i++) {
-            out += a[i] + " ";
-        }
-        if (b > 0) {
-            for (let i = 0; i < b; i++) {
-                out += a[i] + " ";
-            }
-        }
-    } else {
-        let diff = b - a.length;
-        for (let i = diff; i < a.length; i++) {
-            out += a[i] + " ";
-        }
-        for (let i = 0; i < diff; i++) {
-            out += a[i] + " ";
-        }
-    }
-    console.log(out);
+function solve(a) {
+    a.sort();
+    console.log(a)
 }
 
-solve([51, 47, 32, 61, 21], 2);
-solve([32, 21, 61, 1], 4);
-solve([2, 4, 15, 31], 10);
+solve([1, 4, 3, 2])
