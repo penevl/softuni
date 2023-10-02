41875fc9f5c2afd395017cc4b815eed0efa9efa2
penevl <penevl@protonmail.com>
2023-10-02 09:25:46 +0300

    Sum digits

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,19 +1,8 @@
 function solve(n) {
-    var tries = 0;
-    const user = n[0];
-    const passwd = user.split("").reverse().join("");
-    n.splice(0, 1);
-    n.forEach((x) => {
-        tries++;
-        if (tries >= 4) {
-            console.log(`User ${user} blocked!`);
-            return;
-        }
-        if (x == passwd) {
-            console.log(`User ${user} logged in.`);
-            return;
-        } else {
-            console.log("Incorrect password. Try again.");
-        }
-    });
+    let sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n = Math.floor(n / 10);
+    }
+    console.log(sum);
 }
