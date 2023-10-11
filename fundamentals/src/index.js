95328e3f8fcdbf536980cd3bd5051de1c97299d1
penevl <penevl@protonmail.com>
2023-10-11 13:49:00 +0300

    Equal Sums

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,35 +1,33 @@
 /**
  *
- * @param {Number[]} a
+ * @param {Number[]} arr
  */
-function solve(a) {
-    const largestIndex = (arr) => {
-        let i;
-        let max = arr[0];
-        let maxIndex = 0;
-        for (i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                maxIndex = i;
-            }
+function solve(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let sumRight = 0;
+        let sumLeft = 0;
+        for (let x = 0; x < i; x++) {
+            sumLeft += arr[x];
         }
-
-        return maxIndex;
-    };
-    a.splice(0, largestIndex(a));
-    const filteredArray = [...new Set(a)];
-    let out = "";
-    filteredArray.sort((a, b) => {
-        return a - b;
-    });
-    filteredArray.reverse();
-    for (let i = 0; i < 3; i++) {
-        if (filteredArray[i]) out += filteredArray[i] + " ";
+        for (let x = i + 1; x < arr.length; x++) {
+            sumRight += arr[x];
+        }
+        if (sumLeft == 0 && sumRight == 0) {
+            console.log(i);
+            break;
+        }
+        if (sumLeft == sumRight) {
+            console.log(i);
+            break;
+        }
+        if (i == arr.length - 1) {
+            console.log("no");
+        }
     }
-    console.log(out);
 }
 
-solve([1, 4, 3, 2]);
-solve([14, 24, 3, 19, 15, 17]);
-solve([41, 41, 34, 20]);
-solve([27, 19, 42, 2, 13, 45, 48]);
+solve([1, 2, 3, 3]);
+solve([1, 2]);
+solve([1]);
+solve([1, 2, 3]);
+solve([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4]);
