e91bc582474953fb4775181123f2772c7864c76b
penevl <penevl@protonmail.com>
2023-10-20 21:11:15 +0300

    Password validator

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,7 +1,22 @@
 function solve(a) {
-    a.forEach((number) => {
-        const numberStr = number.toString();
-        const reversedNumberStr = numberStr.split("").reverse().join("");
-        console.log(numberStr === reversedNumberStr);
-    });
+    let correct = true;
+    if (a.length < 6 || a.length > 10) {
+        console.log("Password must be between 6 and 10 characters");
+        correct = false;
+    }
+
+    if (!/^[a-zA-Z0-9]+$/.test(a)) {
+        console.log("Password must consist only of letters and digits");
+        correct = false;
+    }
+
+    const digitCount = a.split("").filter((char) => /\d/.test(char)).length;
+    if (digitCount < 2) {
+        console.log("Password must have at least 2 digits");
+        correct = false;
+    }
+
+    if (!correct) return;
+
+    console.log("Password is valid");
 }
