66c8888408cc50ce0fe4f5de40cea6093471d1e4
penevl <penevl@protonmail.com>
2023-10-20 21:13:34 +0300

    NxN matrix

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,22 +1,10 @@
 function solve(a) {
-    let correct = true;
-    if (a.length < 6 || a.length > 10) {
-        console.log("Password must be between 6 and 10 characters");
-        correct = false;
+    for (let i = 0; i < a; i++) {
+        let row = "";
+        for (let j = 0; j < a; j++) {
+            row += a + " ";
+        }
+        console.log(row);
     }
-
-    if (!/^[a-zA-Z0-9]+$/.test(a)) {
-        console.log("Password must consist only of letters and digits");
-        correct = false;
-    }
-
-    const digitCount = a.split("").filter((char) => /\d/.test(char)).length;
-    if (digitCount < 2) {
-        console.log("Password must have at least 2 digits");
-        correct = false;
-    }
-
-    if (!correct) return;
-
-    console.log("Password is valid");
 }
+
