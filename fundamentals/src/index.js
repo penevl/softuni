0aa2fbcb3200e5bfaa711c830da99f0e17049767
penevl <penevl@protonmail.com>
2023-10-20 20:58:41 +0300

    Palindrome integers

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,17 +1,7 @@
 function solve(a) {
-    let sumEven = 0;
-    let sumOdd = 0;
-
-    const numStr = a.toString();
-
-    for (let i = 0; i < numStr.length; i++) {
-        const digit = parseInt(numStr[i]);
-
-        if (digit % 2 === 0) {
-            sumEven += digit;
-        } else {
-            sumOdd += digit;
-        }
-    }
-    console.log(`Odd sum = ${sumOdd}, Even sum = ${sumEven}`);
+    a.forEach((number) => {
+        const numberStr = number.toString();
+        const reversedNumberStr = numberStr.split("").reverse().join("");
+        console.log(numberStr === reversedNumberStr);
+    });
 }
